5771ce61c3568aca4d1411eca19fc1c252db74a8
PilotTim136 <102730061+PilotTim136@users.noreply.github.com>
2025-10-15 15:28:12 +0200

    CSS UI changes

---
+++ main.js
@@ -0,0 +1,8 @@
+const crcVer = "1.1";
+
+document.body.innerHTML += `
+<div id="footer">
+    Version: ${crcVer}<br>
+    Made by <code>PilotTim136</code><br>
+    Github: <a href="https://github.com/PilotTim136/CreateRecipeCreator">CreateRecipeCreator</a>
+</div>`;
